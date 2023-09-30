0c0375e141b83503f46f360333b14250dc17dcc4
Tafreed <atafreed1@gmail.com>
2023-09-30 03:54:21 -0700

    added sound for horn and light for car, also outlined the objects green which can change upon collision

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 import pandas as pd
 import math
+import pygame.mixer
+
 
 pygame.init()
+pygame.mixer.init()
 
 data = pd.read_csv("data_normalized.csv")
 
 # Set up the drawing window
 screen = pygame.display.set_mode([1200, 1200])
-
-
 
 # Run until the user asks to quit
 running = True
@@ -25,6 +26,32 @@ dely = 0
 t = 0
 delt = 0
 
+# Initial glow radius and color of objects
+glow_radius = 5
+
+glow_objects_1 = ('#1A5D1A')
+glow_objects_2 = ('#1A5D1A')
+glow_objects_3 = ('#1A5D1A')
+glow_objects_4 = ('#1A5D1A')
+
+
+# of car
+glow_width = 5
+glow_car = (255, 255, 255)  # White glow color
+
+# Light of car
+glow_enabled = True
+
+# Sound of horn
+sound_enabled = False
+sound = pygame.mixer.Sound('horn.wav')
+
+
+blue = (0, 0, 255)    # Blue
+red = (255, 0, 0)    # Red
+green = (0, 255, 0)    # Green
+yellow = (255, 255, 0)  # Yellow
+
 while running:
 
     # Did the user click the window close button?
@@ -33,12 +60,24 @@ while running:
             running = False
 
     # Fill the background with white
-    
-
+    screen.fill(('#AEC3AE'))
 
     for index, row in data.iterrows():
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, (0, 0, 255), ((x*6) + 500, (y*6) + 500), 10)
+        # Draw the square
+        square_size = 20  # Size of the square
+        square_color = (0, 0, 0)  # Blue square color
+        square_x = (x * 6) + 500
+        square_y = (y * 6) + 500
+        pygame.draw.rect(screen, square_color, (square_x, square_y, square_size, square_size))
+
+        # Draw the glow effect around the square using a rectangle
+        if glow_enabled:
+            glow_rect = pygame.Rect(square_x - glow_width, square_y - glow_width, square_size + 2 * glow_width, square_size + 2 * glow_width)
+            pygame.draw.rect(screen, glow_car, glow_rect, 3) 
+        
+        if sound_enabled:
+            sound.play()
+        
         x1_object = x + ((row['FirstObjectDistance_X']* math.cos(theta)) - (row['FirstObjectDistance_Y'] * math.sin(theta)))
         y1_object = y + ((row['FirstObjectDistance_X']* math.sin(theta)) - (row['FirstObjectDistance_Y'] * math.cos(theta)))
         x2_object = x + ((row['SecondObjectDistance_X']* math.cos(theta)) - (row['SecondObjectDistance_Y'] * math.sin(theta)))
@@ -47,13 +86,20 @@ while running:
         y3_object = y + ((row['ThirdObjectDistance_X']* math.sin(theta)) - (row['ThirdObjectDistance_Y'] * math.cos(theta)))
         x4_object = x + ((row['FourthObjectDistance_X']* math.cos(theta)) - (row['FourthObjectDistance_Y'] * math.sin(theta)))
         y4_object = y + ((row['FourthObjectDistance_X']* math.sin(theta)) - (row['FourthObjectDistance_Y'] * math.cos(theta)))
-        pygame.draw.circle(screen, (0, 255, 0), (x1_object*6 +500, y1_object*6 + 500), 5)
-        pygame.draw.circle(screen, (255, 0, 0), (x2_object*6 +500, y2_object*6 + 500), 5)
-        pygame.draw.circle(screen, (0, 255, 255), (x3_object*6 +500, y3_object*6 + 500), 5)
-        pygame.draw.circle(screen, (255, 255, 0), (x4_object*6 +500, y4_object*6 + 500), 5)
-        # pygame.draw.circle(screen, (0, 255, 0), ((x + row['SecondObjectDistance_X'])*6 +500, (y + row['SecondObjectDistance_Y'])*6 + 500), 5)
-        # pygame.draw.circle(screen, (255, 255, 0), ((x*6) + 500 + row['ThirdObjectDistance_X']*6, (y*6) + 500 + row['ThirdObjectDistance_Y']*6), 5)
-        # pygame.draw.circle(screen, (0, 255, 255), ((x*6) + 500 + (row['FourthObjectDistance_X']*6), (y*6) + 500 + (row['FourthObjectDistance_Y']*6)), 5)
+
+        # Draw the objects with inner color and glow effect
+        inner_circle_radius = 5
+        pygame.draw.circle(screen, ('#CECE5A') , (x1_object*6 + 500, y1_object*6 + 500), inner_circle_radius)  # Inner circle
+        pygame.draw.circle(screen, glow_objects_1, (x1_object*6 + 500, y1_object*6 + 500), inner_circle_radius + glow_radius, 3)  # Glow effect
+        pygame.draw.circle(screen, ('#FFE17B') , (x2_object*6 + 500, y2_object*6 + 500), inner_circle_radius)  # Inner circle
+        pygame.draw.circle(screen, glow_objects_2, (x2_object*6 + 500, y2_object*6 + 500), inner_circle_radius + glow_radius, 3)  # Glow effect
+        pygame.draw.circle(screen, ('#FD8D14'), (x3_object*6 + 500, y3_object*6 + 500), inner_circle_radius)  # Inner circle
+        pygame.draw.circle(screen, glow_objects_3, (x3_object*6 + 500, y3_object*6 + 500), inner_circle_radius + glow_radius, 3)  # Glow effect
+        pygame.draw.circle(screen, ('#C51605'), (x4_object*6 + 500, y4_object*6 + 500), inner_circle_radius)  # Inner circle
+        pygame.draw.circle(screen, glow_objects_4, (x4_object*6 + 500, y4_object*6 + 500), inner_circle_radius + glow_radius, 3)  # Glow effect
+
+        # Repeat the same process for other objects (x2_object, x3_object, x4_object)
+
         if (index < (len(data) - 1)):
             delt = data.iloc[index + 1]['Timestamp'] - row['Timestamp']
         print('delt', delt)
@@ -65,7 +111,8 @@ while running:
         print(x, y)
         pygame.time.delay(50)
         pygame.display.flip()
-    screen.fill((255, 255, 255))
+        screen.fill(('#AEC3AE'))
+
     x = 0
     y = 0
     theta = 0
@@ -73,7 +120,6 @@ while running:
     print('game end')
 
     # Flip the display
-    
 
 # Done! Time to quit.
 pygame.quit()
